58d19a2288687f284b25bf3cc00de6016768d772
Erick Araya <95132759+Francolin09@users.noreply.github.com>
2025-10-29 23:30:29 -0300

    02-useState y tipos

--- src/01-useState/TrafficLight.tsx
+++ src/01-useState/TrafficLight.tsx
@@ -1,4 +1,5 @@
-//1 creamos este componente que son puras clases de tailwind que hacen un semaforo con unos botones
+//1 acá todo bien pero tenemos un problemita, y es que a pesar de tener tres colores, podriamos poner cualquier cosa y ts no se quejaria 
+//entonces para evitar eso crearemos unos tipos
 
 import { useState } from "react";
 
@@ -8,33 +9,34 @@ const colors = {
     green:'bg-green-500 animate-pulse'
 }
 
+//2 creamos los tipos en indicamos sus posibles valores
+type TrafficLightColor = 'red'|'green'|'yellow'
+//2.5 esta forma de arriba es totalmente válida pero algunos tontitos dicen, ya pero si agrego un color arriba, tengo que agregarlo aca y paja.
+//para esos tontitos está este metodo avanzado
+type TrafficLightColors = keyof typeof colors; //aca decimos que la variable será una de las llaves de el tipo colors, entonces si usaramos esta
+//despues solo tendriamos que agregar el color en el objeto de colores y fin.
+
 export const TrafficLight = () => {
 
-    //2 ahora bien, recordando siempre que queremos que algo cambie su valor y verlo reflejado necesitaremos algo con lo que
-    //identificar su estado, para eso usamos el useState
+    const [light, setLight] =useState<TrafficLightColor>('red'); //3 ahora ponemos eluseState será del tipo que creamos
 
-    const [light, setLight] =useState('red'); //3 entonces lo primero creamos el useState con valor default de red
 
-    //3 luego lo que necesitamos es poder manejar las clases un poco mas dinamicamente, para eso primero nos crearemos un objeto
-    //que tendrá para cada color la clase de tailwind correspondiente.
-
-    //5 finalmente creamos una funcion para manejar el color con el setLight y la pasamos en cada uno de los botones con el valor correspondiente
-    const handleChangeColor = (color: string) => {
+    const handleChangeColor = (color: TrafficLightColor) => { //4 y acá el color igual
         setLight((prev) => {
             console.log(prev)
             return color;
         })
     }
+    //5 hecho e eso ahora solo podremos pasarle alguno de los valores definidos en el tipo.
     return (
       <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
         <div className="flex flex-col items-center space-y-8">
-            {/* 4 Hecho eso ahora podemos decir que si light es red, que es cierto porque el valor por defecto, entonces nos retorne y agregue
-            la clase correspondiente al red, y de no ser, entonces que sea gris. */}
+       
           <div className={`w-32 h-32 ${light === 'red' ? colors[light]: 'bg-gray-500'} rounded-full`}></div>
           <div className={`w-32 h-32 ${light === 'yellow' ? colors[light]: 'bg-gray-500'} rounded-full`}></div>
           <div className={`w-32 h-32 ${light === 'green' ? colors[light]: 'bg-gray-500'} rounded-full`}></div>
   
-          {/* Botón para cambiar el estado de la luz */}
+   
           <div className="flex gap-2">
             <button
               className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer" onClick={ () => handleChangeColor('red')}>
