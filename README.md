e4640648b7f60b5ac36984e217b41b7ad84f7b7f
Erick Araya <95132759+Francolin09@users.noreply.github.com>
2025-11-02 18:10:56 -0300

    08 Lista de tareas con useState

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-Ahora veremos de una forma un poco más profunda el hook useRef, para eso nos creamos una carpeta 04-useRef y dentro un archivo focusScreen.tsx
+En esta oportunidad veremos el hook useReducer 
+asi que nos creamos 05-useReducer y luego nos oegaremos un codigo que está en un gist 
+y lo pegaremos en TaskApp.tsx
+las cosas que pegamos necesitan shadcn para eso tenemos que instalarlo y hacer algunas configuraciones
+primero agregar esto en tsconfig.json:
+  "compilerOptions":{
+    "baseUrl":".",
+    "paths":{
+      "@/*": ["./src/*"]
+    }
+  }
+  Tambien tenemos que agregarlo en tsconfig.app.json
+
+  luego:
+  npm install -D @types/node
+
+  Hecho esto debemos configurar el vite.config.ts donde agregaremos esto:
+  import path from "path"
+  import tailwindcss from "@tailwindcss/vite"
+
+  export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
+Hecho todo ejecutamos el npx shadcn@latest add y elegimos lo que queremos usar.
+ahora vamos al archivo Taskapp
--- src/04-useRef/FocusScreen.tsx
+++ src/04-useRef/FocusScreen.tsx
@@ -1,12 +1,8 @@
-
-//1 ya, que es lo que queremos mostrar acá
-//a veces hay acciones demasiado simples que al cambiar no queremos que nuestro componente vuelva a renderizar, porque no es necesario
-
 import { useRef } from "react"
 
-//y justo ese es el caso de un cambio de foco 
+
 export const FocusScreen = () => {
-    //2 asi que primero nos creamos una variable de useRef. como sabemos que la utilizaremos en el input podemos tiparlo como inputelement
+
     const inputRef =useRef<HTMLInputElement>(null);
 
     const handleClick = () => {
@@ -16,11 +12,10 @@ export const FocusScreen = () => {
   return (
     <div className="bg-gradient flex flex-col gap-4">
         <h1 className="text-2xl font-thin text-white">Focus Screen</h1>
-        {/*3 Este atributo ref en react le dice cuando montes este elemento en el dom(el input) guarda su referencia en inputRef */}
+
         <input type="text" className="bg-white text-black px-4 py-2 rounded-md" autoFocus ref={inputRef} />
         
-        {/*4 Finalmente en el boton le pasamos la funcion que creamos que lo unico que hace es tomar el input y hacerle un select al contenido */}
-        {/* Lo grandioso de esto es que no se vuelve a renderizar. fin  */}
+
         <button
         className="bg-blue-800 text-white px-4 py-2 rounded-md cursor-pointer"
         onClick={handleClick}
